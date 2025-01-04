c934996e4680cae9d0537ca7b23da83d6f03e86f
vemashop <vemasaikumar328@gmail.com>
2025-01-04 23:51:19 +0530

    modified file 07-logs.sh

--- 07-logs.sh
+++ 07-logs.sh
@@ -7,7 +7,7 @@ USERID=$(id -u)
 LOG_FOLDER="/var/log/shellscripts-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%D-%H:%M:%S) #date prints as (month/day/year)-hour:minute:second
-LOG_FILENAME="$LOG_FOLDER/$LOG_FILE:$TIME_STAMP" 
+LOG_FILENAME=$LOG_FOLDER/$LOG_FILE:$TIME_STAMP
 USERNAME=$USER
 
 functionqm(){
