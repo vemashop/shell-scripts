725cdc76feb2e0884e313ea5bdbcf8e3beb1c896
vemashop <vemasaikumar328@gmail.com>
2025-01-06 15:34:49 +0530

    modifying 09 file

--- 09-backup.sh
+++ 09-backup.sh
@@ -9,7 +9,7 @@ Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER=/home/vemasaikumar/saishellscripts-logs
 TIMESTAMP=$(date +%d/%m/%Y-%H:%M:%S)
-FILE=$(echo $0)
+FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 LOG_FILE_NAME="$LOGS_FOLDER/$FILE-$TIMESTAMP.log"
 
 mkdir -p /home/vemasaikumar/saishellscripts-logs
