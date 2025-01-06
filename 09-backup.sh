fa54006f621dc92bcdd5a0369d06e2fff80925c3
vemashop <vemasaikumar328@gmail.com>
2025-01-06 23:06:56 +0530

    modifying 09 file

--- 09-backup.sh
+++ 09-backup.sh
@@ -38,7 +38,7 @@ FILES=$(find $SOURCE_DIR -name "*.logs" -mtime +$DAYS)
 if [ -n "$FILES" ]; then
  echo "The files are: $FILES"
  ZIP_FILE="$DESTINATION_DIR/zipped-files-$TIMESTAMP.zip"
- find $SOURCE_DIR -name "*.logs" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+ find $SOURCE_DIR -name "*.logs" -mtime +$DAYS | zip -@ $ZIP_FILE
   if [ -f $ZIP_FILE ]
    then 
     echo "Successfully created zip file for files older than $DAYS"
