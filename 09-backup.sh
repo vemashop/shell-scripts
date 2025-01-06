f0a76fc2eb9bf40cf8efd68989cfe181da8ee038
vemashop <vemasaikumar328@gmail.com>
2025-01-06 13:09:26 +0530

    modifying 09 file

--- 09-backup.sh
+++ 09-backup.sh
@@ -4,7 +4,11 @@ SOURCE_DIR=$1
 DESTINATION_DIR=$2
 DAYS=${3:-14}
 
-LOGS_FOLDER="/home/$USER/shellscript-logs"
+LOGS_FOLDER=/home/$USER/shellscript-logs
+TIMESTAMP=$(date +%d/%m/%Y-%H:%M:%S)
+FILE=$(echo $0 | cut -d "." -f1)
+LOG_FILE_NAME=$LOGS_FOLDER/$FILE-$TIMESTAMP.log
 
-cat LOGS_FOLDER
+
+
 
