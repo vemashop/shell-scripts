27f462fd4cc8baeb5bc7a3a1e706f0aae2746de1
vemashop <vemasaikumar328@gmail.com>
2025-01-07 23:31:51 +0530

    Modifying 10 file

--- 10-monitor.sh
+++ 10-monitor.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOULD=5
 
 while read -r line
 do
   USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
   PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
-  echo "USAGE:$USAGE , PARTITION:$PARTITION"
+  
+  if [ $USAGE -ge $DISK_THRESHOULD ]
+  then
+   echo "USAGE:$USAGE , PARTITION:$PARTITION"
 done <<< $DISK_USAGE
 
 
