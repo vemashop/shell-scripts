26aa9c60937bd225271d9b0cf290496a161f61e5
vemashop <vemasaikumar328@gmail.com>
2025-01-07 23:25:50 +0530

    Modifying 10 file

--- 10-monitor.sh
+++ 10-monitor.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
 
-PARTITION=$(df -hT | grep xfs | awk -F " " '{print $NF}')
 
-echo "$USAGE , $PARTITION"
+DISK_USAGE=$(dh -hT | grep xfs)
+
+while read -r line
+do
+  USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+  PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+  echo "USAGE:$USAGE , PARTITION:$PARTITION"
+done <<< $DISK_USAGE
+
+
 
