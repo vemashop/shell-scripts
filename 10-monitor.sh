22fa1ef442fcac0ce2c50d1ea931042aeca98621
vemashop <vemasaikumar328@gmail.com>
2025-01-07 23:17:07 +0530

    Modifying 10 file

--- 10-monitor.sh
+++ 10-monitor.sh
@@ -4,5 +4,5 @@ USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
 
 PARTITION=$(df -hT | grep xfs | awk -F " " '{print $NF}')
 
-echo "$USAGE - $PARTITION"
+echo "$USAGE" && echo "$PARTITION"
 
