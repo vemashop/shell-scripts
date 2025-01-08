1f8541b925482653658fcf9b42255508e48bc239
vemashop <vemasaikumar328@gmail.com>
2025-01-08 12:06:18 +0530

    Modifying 11 file

--- 11-monitor.sh
+++ 11-monitor.sh
@@ -9,7 +9,12 @@ while read -r line
 do
   PARTITION=$(echo $line | awk -F " " '{print $NF}')
   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-  echo "Partition:$PARTITION and USAGE:$USAGE"
+#   echo "Partition:$PARTITION and USAGE:$USAGE"
+  if [ $USAGE -ge $DISK_THRESHOULD ]; then
+      echo "Partition:$PARTITION and USAGE:$USAGE
+  fi    
+     
+
 done <<< $DISK_USAGE 
 
    
