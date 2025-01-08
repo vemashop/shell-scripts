ae949d5e75af302fefd276ab78f19b584edf4c16
vemashop <vemasaikumar328@gmail.com>
2025-01-08 11:53:58 +0530

    Modifying 11 file

--- 11-monitor.sh
+++ 11-monitor.sh
@@ -6,8 +6,11 @@ DISK_THRESHOULD=5
 PARTITION=$(df -hT | grep xfs | awk -F " " '{print $NF}')
 USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
 
- 
+while read -r line
+do
 echo "Partition: $PARTITION and USAGE:$USAGE"
+done <<< $DISK_USAGE 
+
    
 
 
